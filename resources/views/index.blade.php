6f4cb56e1df141c8035e5e1c630b9da61c3abedd
gilmour59 <gilmouralmalbisdev@gmail.com>
2018-09-11 16:10:48 +0800

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="text-center mt-5">
+        <h2>Organizer Test</h2>
+    </div>
+    
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <style>
+            * {
+                background-color: #fff;
+                color: black;
+                font-family: 'Raleway', sans-serif !important;
+                font-weight: 400 !important;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
