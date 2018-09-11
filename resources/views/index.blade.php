89e826b91a76e84be5d3b9663d828cad87ac8578
gilmour59 <gilmouralmalbisdev@gmail.com>
2018-09-11 16:31:49 +0800

    111

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,20 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="text-center mt-5">
+    <div class="text-center my-4">
         <h2>Organizer Test</h2>
     </div>
-    
+    <div class="card">
+        <div class="card-header font-weight-bold">
+            Search
+        </div>
+        <div class="card-body">
+            <form method="POST" action="">
+                @csrf
+                <div class="form-group">
+                    <input id="search" name="search" class="form-control offset-3 col-sm-6" type="text">
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
